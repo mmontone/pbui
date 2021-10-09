bf674e13215f24117d631c4a905e1ebe7c4e2542
Mariano Montone <marianomontone@gmail.com>
2021-10-09 17:03:37 -0300

    Housekeeping

--- pbui-examples.el
+++
@@ -1,7 +0,0 @@
-(require 'presentations)
-
-(insert (presentation 22 "twentytwo"))twentytwo
-
-(insert (presentation "/home/marian/src/test.lisp" "test file" 'file))test file
-
-(insert (presentation "/home/marian/src" "source folder" 'directory))source folder
