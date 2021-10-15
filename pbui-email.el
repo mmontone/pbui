68e8fa43662a89ff04e4515035c145085060aecf
Mariano Montone <marianomontone@gmail.com>
2021-10-15 16:23:27 -0300

    Command for setting an email message 'to' field

--- pbui-email.el
+++ pbui-email.el
@@ -12,4 +12,19 @@
   (dolist (file files)
     (mml-attach-file file)))
 
+(def-presentation-command (email-commands:set-email-to-field
+			   :title "Set email message 'to' field"
+			   :description "Set email message 'to' field"
+			   :applyable-when (lambda (ps)
+					     (and mml-mode
+						  (cl-some (lambda (p)
+							     (eql (presentation-type p) 'email))
+							   ps))))
+  ((emails email))
+  (message-goto-to)
+  (dolist (email emails)
+    (insert email)
+    (insert ", "))
+  (delete-region (- (point) 2) (point)))
+
 (provide 'pbui-email)
